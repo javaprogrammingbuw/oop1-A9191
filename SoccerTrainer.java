9c4b817948080f05ffb513db1b2c27d12cb195b0
afshin.Ghasemi@uni-weimar.de <yepu7258@in.uni-weimar.de>
2019-03-25 15:38:11 +0100

    <COMPLETE>

---
+++ SoccerTrainer.java
@@ -0,0 +1,15 @@
+package Soccer;
+
+public class SoccerTrainer {
+	
+	private String trainerName;
+	
+	public SoccerTrainer(String name) {
+		trainerName=name;
+	}
+	
+	public String toString() {
+		return trainerName;
+	}
+
+}
